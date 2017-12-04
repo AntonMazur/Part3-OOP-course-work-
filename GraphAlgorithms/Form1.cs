122f7c751337e08ce23add92fe32283a6cc68e69
Антон Лесников <toni.lesnikov.98@mail.ru>
2017-12-04 15:38:12 +0300

    Initial commit

---
+++ GraphAlgorithms/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GraphAlgorithms.Graphs;
+
+namespace GraphAlgorithms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MinHamilCycleFinder min = new MinHamilCycleFinder(getAdjacencyMatrix());
+            MessageBox.Show(min.testMethod()[0].ToString());
+        }
+
+        private int[,] getAdjacencyMatrix()
+        {
+            int[,] adjcMatrix = new int[6, 6];
+            for (int i = 1; i <= 6; i++)
+            {
+                for (int j = 1; j <=6; j++)
+                {
+                    string tBoxContent = getTextBoxContent(i, j);
+                    if (tBoxContent == "")
+                    {
+                        adjcMatrix[i - 1, j - 1] = -1;
+                        continue;
+                    }
+                    else
+                    {
+                        adjcMatrix[i - 1, j - 1] = int.Parse(tBoxContent);
+                    }                
+                }
+            }
+
+            return adjcMatrix;
+        }
+
+        private string getTextBoxContent(int row, int column)
+        {
+            string text;
+            try
+            {
+                text = ((TextBox)tableLayoutPanel_graph.GetControlFromPosition(column, row)).Text;
+            } catch(Exception e)
+            {
+                text = "";   
+            }
+            return text;
+        }
+    }
+}
---
+++ GraphAlgorithms/Graphs/MinHamilCycleFinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GraphAlgorithms.Graphs
+{
+    class MinHamilCycleFinder
+    {
+        private Vertex[] graph;
+
+        public MinHamilCycleFinder(int[,] adjacMatrix)
+        {
+            initGraph(adjacMatrix);
+        }
+
+        public Vertex[] testMethod()
+        {
+            return graph;
+        }
+
+        private void initGraph(int[,] adjacMatrix)
+        {
+            graph = new Vertex[adjacMatrix.GetLength(0)];
+            Dictionary<Vertex, int> fromVertEdges;
+            int i;
+
+            for (i = 0; i < 6; i++)
+            {
+                graph[i] = new Vertex(i);
+            }
+
+            for (i = 0; i < 6; i++)
+            {
+                fromVertEdges = new Dictionary<Vertex, int>();
+
+                for (int j = 0; j < 6; j++)
+                {
+                    if (adjacMatrix[i,j] != -1)
+                    {
+                        fromVertEdges.Add(graph[j], adjacMatrix[i, j]);
+                    }
+                }
+
+                graph[i].setAdjcVertexes(fromVertEdges);
+            }
+        }
+    }
+}
